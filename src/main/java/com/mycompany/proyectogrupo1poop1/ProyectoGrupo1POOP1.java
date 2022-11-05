ad9def183d9a4656bc605c4fb3c151334042d8a5
Galo Quizhpe <grqm-2000@hotmail.com>
2022-11-05 17:24:58 -0500

    Proyecto Grupo 1 POO Paralelo 1

---
+++ src/main/java/com/mycompany/proyectogrupo1poop1/ProyectoGrupo1POOP1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.proyectogrupo1poop1;
+
+/**
+ *
+ * @author USER
+ */
+public class ProyectoGrupo1POOP1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
