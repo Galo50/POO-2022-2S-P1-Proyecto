6b2c41fdf86008bd976079f7485cc2b566d3aa6d
Galo Quizhpe <grqm-2000@hotmail.com>
2022-11-05 17:48:45 -0500

    vehiculos incluidos

---
+++ src/main/java/com/mycompany/proyectogrupo1poop1/Vehiculo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectogrupo1poop1;
+
+/**
+ *
+ * @author USER
+ */
+public class Vehiculo {
+    private int year;
+    private String marca;
+    private int llantas;
+    private String combustible;
+
+    public Vehiculo(int year, String marca, int llantas, String combustible) {
+        this.year = year;
+        this.marca = marca;
+        this.llantas = llantas;
+        this.combustible = combustible;
+    }
+    
+    
+}
