ec0978aef786a2ea914a047b5043b80b084c1cf2
Joan Mendoza <107806374+Lighty51015@users.noreply.github.com>
2022-11-26 18:27:15 -0500

    Menu Vendedor Update 1

--- src/main/java/menu/MVendedor.java
+++ src/main/java/menu/MVendedor.java
@@ -4,10 +4,26 @@
  */
 package menu;
 
+import java.util.Scanner;
 /**
  *
  * @author joanm
  */
 public class MVendedor {
-    
+    public static int menuVendedor() {
+        int optionChose;
+        Scanner inputJava = new Scanner(System.in);
+        
+        System.out.println("""
+                           ===============================================
+                                            MENÚ VENDEDOR
+                           ===============================================
+                                             * Opciones *
+                           1. Consultar Vehículos
+                           2. Revisar solicitudes de cotización
+                           
+                           Ingrese el número de la opción a elegir:
+                           """);
+        
+    }
 }
