d007d0b5f0131efadabeb5c3c663925705e1e170
Galo50 <grqm-2000@hotmail.com>
2022-11-26 14:27:33 -0500

    jefe a medias

--- src/main/java/usuarios/JefeTaller.java
+++ src/main/java/usuarios/JefeTaller.java
@@ -5,7 +5,9 @@
 package usuarios;
 
 import java.util.ArrayList;
+import java.util.Random;
 import solicitudes.Solicitud;
+import vehiculos.Vehiculo;
 
 /**
  *
@@ -22,4 +24,14 @@ public class JefeTaller extends Usuario{
     public ArrayList getCertificaciones() {
         return this.certificacionTecnica;
     }
+public void asignarMecanico(Vehiculo carro, ArrayList<Mecanico> listaMecanicos) {
+        Random rand = new Random();
+        int randomPosition = rand.nextInt(listaMecanicos.size());
+        ArrayList<Vehiculo> carrosMecanico= listaMecanicos.get(randomPosition).getCarrosMecanico();
+        carrosMecanico.add(carro);
+        listaMecanicos.get(randomPosition).setCarrosMecanico(carrosMecanico);
+    }
+    public void vehiculosMantenimiento(ArrayList<Vehiculo> carrosMantenimiento){
+        System.out.println("Actualmente existen "+carrosMantenimiento.size()+"vehiculos en el taller");
+        }
 }
--- src/main/java/usuarios/Mecanico.java
+++ src/main/java/usuarios/Mecanico.java
@@ -5,15 +5,29 @@
 package usuarios;
 
 import java.util.ArrayList;
+import vehiculos.Vehiculo;
 
 /**
  *
  * @author USER
  */
 public class Mecanico extends Usuario{
+    
+    private ArrayList<Vehiculo> carrosMecanico;
 
-    public Mecanico(String userName, String contraseña, String nombres, String apellidos, ArrayList BanEntrada) {
-        super(userName, contraseña, nombres, apellidos, BanEntrada); 
+    public Mecanico(ArrayList<Vehiculo> carrosMecanico, String userName, String password, String nombres, String apellidos, ArrayList solicitudes) {
+        super(userName, password, nombres, apellidos, solicitudes);
+        this.carrosMecanico = carrosMecanico;
     }
+
+    public ArrayList<Vehiculo> getCarrosMecanico() {
+        return carrosMecanico;
+    }
+
+    public void setCarrosMecanico(ArrayList<Vehiculo> carrosMecanico) {
+        this.carrosMecanico = carrosMecanico;
+    }
+            
+    
     
 }
