3c0e8c9174d9b5be04e992b9b0f0fd17c6aa078b
Christopher Díaz <nealthmc@gmail.com>
2022-12-01 17:20:10 -0500

    eliminar guardar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -68,9 +68,6 @@ public class Main {
             if (!AppState.getRunApp()) {
                 Print.brindarDespedida();
             }
-            
-            Guardar.usuarios(usuarios);
-            Guardar.vehiculos(vehiculos);
         }
     }
 }
