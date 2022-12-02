1edc5bf505ed603fd42fc678c2a1878551aaff1c
Galo50 <grqm-2000@hotmail.com>
2022-12-01 23:52:08 -0500

    s.respuesta

--- src/main/java/solicitudes/SRespuesta.java
+++ src/main/java/solicitudes/SRespuesta.java
@@ -57,7 +57,27 @@ public class SRespuesta extends Solicitud{
             }
         
         if(carro != null){ 
-    System.out.println("Estimado "+destinatario.getNombres()+" "+destinatario.getApellidos()+"\n Por medio de la presente se le informa que su solicitud de "+tramite+" fue aceptada y este el precio del vehiculo"+carro.getPrecio());        
+            if(carro instanceof Automovil == true){
+                Automovil carro2 =(Automovil) carro;
+            System.out.println("Estimado "+destinatario.getNombres()+" "+destinatario.getApellidos()+"\n Por medio de la presente se le informa que su solicitud de "+tramite+" fue aceptada y este el precio del vehiculo"+carro2.getPrecio()        
+       +"marca " +carro2.getMarca()+"modelo"+carro2.getModelo()+"Año "+carro2.getYear()+"combustible"+ carro2.getCombustible()+"Convertible"+carro2.getConvertible()+"Asientos"+carro2.getAsientos());
+            }
+            if(carro instanceof Camion == true){
+            Camion carro2 =(Camion) carro;
+            System.out.println("Estimado "+destinatario.getNombres()+" "+destinatario.getApellidos()+"\n Por medio de la presente se le informa que su solicitud de "+tramite+" fue aceptada y este el precio del vehiculo"+carro2.getPrecio()        
+       +"marca " +carro2.getMarca()+"modelo"+carro2.getModelo()+"Año "+carro2.getYear()+"combustible"+ carro2.getCombustible()+"Ejes"+carro2.getEjes()+"Capacidad de carga "+carro2.getCarga());
+            }
+            if(carro instanceof Tractor == true){
+            Tractor carro2 =(Tractor) carro;
+            System.out.println("Estimado "+destinatario.getNombres()+" "+destinatario.getApellidos()+"\n Por medio de la presente se le informa que su solicitud de "+tramite+" fue aceptada y este el precio del vehiculo"+carro2.getPrecio()        
+       +"marca " +carro2.getMarca()+"modelo"+carro2.getModelo()+"Año "+carro2.getYear()+"combustible"+ carro2.getCombustible()+"Agricola"+carro2.getAgricola()+"tipo de tranmision"+carro2.getTransmision());
+            }
+            if(carro instanceof Motocicleta == true){
+            Motocicleta carro2 =(Motocicleta) carro;
+            System.out.println("Estimado "+destinatario.getNombres()+" "+destinatario.getApellidos()+"\n Por medio de la presente se le informa que su solicitud de "+tramite+" fue aceptada y este el precio del vehiculo"+carro2.getPrecio()        
+       +"marca " +carro2.getMarca()+"modelo"+carro2.getModelo()+"Año "+carro2.getYear()+"combustible"+ carro2.getCombustible()+"llantas"+carro2.getLlantas()+"tipo de Motocicleta"+carro2.getTipoMoto());
+            }
+          
         }
     }   
 }
